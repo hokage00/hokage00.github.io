ae835abcf6c1f9c187870ae6381d1ffafd38ba1b
hokage00 <ruvan6763@gmail.com>
2021-10-01 20:41:48 +0800

    js for id password

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -3,5 +3,19 @@ function transformUpperCase(_val){
 }
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
-elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
+elNamaPengguna.value = transformUpperCase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKatalaluan = document.getElementById("kataLaluan")
+    let pass_val = elKatalaluan.value
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+
+    }else{
+        alert("Password too short!")
+    }
+}
+
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword
