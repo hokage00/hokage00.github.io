e4851c5f85706d9d930c2d2649b6ba6ce68187c6
hokage00 <ruvan6763@gmail.com>
2021-10-04 21:38:45 +0800

    video

--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -1,36 +1,33 @@
-function calculateDistance(lat, lon){
-    let depulze = haversine(lat, lon,2.922561,101.650965)
-    let pyramid = haversine(lat, lon,3.073065,101.607787)
-    let klcc = haversine(lat, lon,3.158761,101.714524)
+function calculateDistances(lat, lon){
+    let dpulze = haversine(lat,lon,2.922561,101.650965)
+    let pyramid = haversine(lat,lon,3.073065,101.607787)
+    let klcc = haversine(lat,lon,3.158761,101.714524)
 
-    return [depulze,pyramid,klcc]
-
+    return [dpulze,pyramid,klcc]
 }
 
 let elLocate = document.getElementById("locate")
 elLocate.addEventListener("click", function(){
- if(navigator.geolocation){
- navigator.geolocation.getCurrentPosition(function(position){
-     let elLat = document.getElementsById("lat")
-     let elLong = document.getElementsById("long")
-     let elDepulze = document.getElementsById("depulze")
-     let elKlcc = document.getElementsById("klcc")
-     let elSunway = document.getElementsById("sunway")
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            let elLat = document.getElementById("lat")
+            let elLong = document.getElementById("long")
+            let elDpulze =document.getElementById("depulze")
+            let elKlcc = document.getElementById("klcc")
+            let elSunway = document.getElementById("sunway")
 
-    let userLat = position.coords.latitude
-    let userLong = position.coords.longitude
+            let userLat = position.coords.latitude
+            let userLong = position.coords.longitude
 
-    let distance = calculateDistance(userLat,userLong)
+            let distances = calculateDistances(userLat, userLong)
 
-    elLat.innerHTML = "Your Latitude" + userLat
-    elLong.innerHTML = "Your Longitude" + userLong
-    elDepulze.innerHTML = "Distance to Dpluze, Cyberjaya is" + distance[0] + "km"
-    elKlcc.innerHTML = "Distance to Klcc is" + distance[2] + "km"
-    elSunway.innerHTML = "Distance to Sunway Pyramid is" + distance[1] + 'km'
-
-    })
-
-}else{
-     alert("Geolocation is not supported")
+            elLat.innerHTML = "Your latitude: " + userLat
+            elLong.innerHTML = "Your longitude: " + userLong
+            elDpulze.innerHTML = "Distance to Dpluze, Cyberjaya is " + distances[0] + "km"
+            elKlcc.innerHTML = "Distance to KLCC is " + distances[2] + "km"
+            elSunway.innerHTML = "Distance to Sunway Pyramid is " + distances[1] + "km"
+        })
+    } else {
+        alert("Geolocation is not supported!")
     }
 })
---
+++ video/js/index.js
@@ -0,0 +1,22 @@
+let video = document.getElementById("myVideo")
+
+let plyBtn = document.getElementById("playBtn")
+playBtn.addEventListener("click", function(){
+    video.play()
+})
+
+let pauseBtn = document.getElementById("pauseBtn")
+pauseBtn.addEventListener("click", function(){
+    video.pause()
+})
+
+let freezeBtn = document.getElementById("freezeBtn")
+freezeBtn.addEventListener("mousedown", function(){
+
+})
+
+
+let freezeBtn = document.getElementById("freezeBtn")
+freezeBtn.addEventListener("mouseup", function(){
+    
+})
